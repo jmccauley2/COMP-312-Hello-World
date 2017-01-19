6f42a7daef8ccff44abc5f99f2cfa00287384520
jmccauley2 <jmccauley2@luc.edu>
2017-01-18 20:09:21 -0600

    Add files via upload
    Added .cpp file

---
+++ Hello-World.cpp
@@ -0,0 +1,15 @@
+// Hello-World.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	system("pause");
+    return 0;
+}
+
